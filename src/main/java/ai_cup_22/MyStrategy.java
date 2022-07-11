769bd88e2732c75b9af9eee0061bd5b8ab601bc5
Semen Tenishchev <tenischev.semen@gmail.com>
2022-07-11 23:28:07 +0300

    simple start

--- src/main/java/ai_cup_22/MyStrategy.java
+++ src/main/java/ai_cup_22/MyStrategy.java
@@ -1,9 +1,21 @@
 package ai_cup_22;
 
+import ai_cup_22.model.*;
+
 public class MyStrategy {
     public MyStrategy(ai_cup_22.model.Constants constants) {}
     public ai_cup_22.model.Order getOrder(ai_cup_22.model.Game game, DebugInterface debugInterface) {
-        return new ai_cup_22.model.Order(new java.util.HashMap<>());
+        java.util.HashMap<Integer, UnitOrder> orders = new java.util.HashMap<>();
+        for (Unit unit : game.getUnits()) {
+            if (unit.getPlayerId() != game.getMyId()) {
+                continue;
+            }
+            orders.put(unit.getId(), new UnitOrder(
+                    new Vec2(-unit.getPosition().getX(), -unit.getPosition().getY()),
+                    new Vec2(-unit.getDirection().getY(), unit.getDirection().getX()),
+                    new ActionOrder.Aim(true)));
+        }
+        return new Order(orders);
     }
     public void debugUpdate(int displayedTick, DebugInterface debugInterface) {}
     public void finish() {}
